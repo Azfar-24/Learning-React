c8e732c8e749e945d496d2761fe219d268267744
Azfar-24 <azfark87@gmail.com>
2022-03-22 00:30:54 +0530

    Completed

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,7 +3,8 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 const Chart = (props) => {
   const dataPointValues = props.dataPoint.map((dataPoint) => dataPoint.value);
-  const totalMax = Math.max(...dataPointValues);
+  const totalMax = dataPointValues.reduce((pre, next) => pre + next, 0);
+  console.log(totalMax);
   return (
     <div className="chart">
       {props.dataPoint.map((dataPoint) => (
@@ -11,7 +12,7 @@ const Chart = (props) => {
           key={Math.random()}
           value={dataPoint.value}
           maxValue={totalMax}
-          label={dataPoint.value}
+          label={dataPoint.label}
         />
       ))}
     </div>
